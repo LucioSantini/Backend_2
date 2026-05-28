e3a6b5955e5413018c342b5df0c282a38d14ee30
Lucio Santini <luciosantini7751@gmail.com>
2026-05-28 03:55:39 -0300

    fix JWT

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import { env } from '../config/env.js'
 
 export const generateTokens =(user)=>{
-    const accessToken = jwt.sign({id: user.id, email: user.email}, env.jwt_secret, {expiresIn:"15m"})
+    const accessToken = jwt.sign({id: user._id, email: user.email, role: user.role}, env.jwt_secret, {expiresIn:"1h"})
     const refreshToken = jwt.sign({id: user.id}, env.session_secret, {expiresIn:"7d"})
 
     return {accessToken,refreshToken}
